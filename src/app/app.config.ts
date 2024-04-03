fc9b43fbeb647224e02da348b87bd67284b116cd
ZecCMachine <zackery35melek@gmail.com>
2024-04-03 12:25:43 -0500

    AngularFire app init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dungeonmaestro5e","appId":"1:771197907114:web:d4713f3ee0339af15d60d3","storageBucket":"dungeonmaestro5e.appspot.com","apiKey":"AIzaSyAkcrnnSNEf6r_xuZvnR1hI0qjCm7l3Nyg","authDomain":"dungeonmaestro5e.firebaseapp.com","messagingSenderId":"771197907114","measurementId":"G-FTVGCM2H1L"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
