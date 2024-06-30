f2d6edbe0de4b96ef44ec865c875691aaa9c93ed
Miguel Campbell <seanmiguel.campbell@gmail.com>
2024-06-29 21:49:36 -0500

    Entire application now backdrops for sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from './shared/auth.service';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { FirestoreService } from './shared/firestore.service';
 import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatBadgeModule, MatDividerModule, MatIconModule, MatSidenavModule, MatGridListModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatBadgeModule, MatDividerModule, MatIconModule, MatSidenavModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -28,6 +29,18 @@ export class AppComponent {
   public async logout() {
     await this.auth.logout();
     this.router.navigate(['login']);
+  }
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  backdropVisible = false;
+
+  toggleSidenav() {
+      this.sidenav.toggle();
+  }
+
+  closeSidenav() {
+      this.sidenav.close();
+      this.backdropVisible = false;
   }
 
 }
