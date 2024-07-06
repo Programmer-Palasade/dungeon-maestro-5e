c7c4209e9ac960404918045b432eac9685ad1e93
ZecCMachine <zackery35melek@gmail.com>
2024-07-05 19:38:42 -0500

    fix for firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,8 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { FirebaseConfig } from './secret';
+
+const FirebaseConfig = {"projectId":"dungeonmaestro5e","appId":"1:771197907114:web:d4713f3ee0339af15d60d3","storageBucket":"dungeonmaestro5e.appspot.com","apiKey":"AIzaSyAkcrnnSNEf6r_xuZvnR1hI0qjCm7l3Nyg","authDomain":"dungeonmaestro5e.firebaseapp.com","messagingSenderId":"771197907114","measurementId":"G-FTVGCM2H1L"};
 
 export const appConfig: ApplicationConfig = {
   providers: [
